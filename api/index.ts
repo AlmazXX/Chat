10d5653cf9272e40f97cd2c14f22d53f261a2d4e
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-22 21:01:51 +0600

    api: Imported cors, added to index

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,11 @@
+import cors from "cors";
 import express from "express";
 import messagesRouter from "./routers/messages";
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/messages", messagesRouter);
 
