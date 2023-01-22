e5479d7d3b4d373f49ed6f206f94fe1903b1b5ea
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-22 21:38:40 +0600

    api: Updated get request of /messages/:id

--- api/routers/messages.ts
+++ api/routers/messages.ts
@@ -3,14 +3,16 @@ import fileDb from "../fileDb";
 
 const messagesRouter = Router();
 
-messagesRouter.get("/", async(req, res) => {
+messagesRouter.get("/", async (req, res) => {
   const messages = await fileDb.getItems();
-  const newMessages = messages.slice(-30)
-  res.send(newMessages)
+  const newMessages = messages.slice(-30);
+  res.send(newMessages);
 });
 
-messagesRouter.get("/:id", (req, res) => {
-  res.send("A single message will be here");
+messagesRouter.get("/:id", async (req, res) => {
+  const messages = await fileDb.getItems();
+  const message = messages.find((m) => m.id === req.params.id);
+  res.send(message);
 });
 
 messagesRouter.post("/", (req, res) => {
