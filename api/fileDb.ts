2b004f98fc525aa781d64c359939c13e9ada0032
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-22 21:20:48 +0600

    api: Created fileDb.ts

---
+++ api/fileDb.ts
@@ -0,0 +1,31 @@
+import { promises as fs } from "fs";
+import { Message, MessageWithoutID } from "./types";
+
+const fileName = "./db.json";
+let data: Message[] = [];
+
+const fileDb = {
+  async init() {
+    try {
+      const fileContents = await fs.readFile(fileName);
+      data = JSON.parse(fileContents.toString());
+    } catch (e) {
+      data = [];
+    }
+  },
+  async getItems() {
+    return data;
+  },
+  async addItem(item: MessageWithoutID) {
+    const id = crypto.randomUUID();
+    const message = { id, ...item };
+    data.push(message);
+    await this.save();
+    return data;
+  },
+  async save() {
+    await fs.writeFile(fileName, JSON.stringify(data));
+  },
+};
+
+export default fileDb;
