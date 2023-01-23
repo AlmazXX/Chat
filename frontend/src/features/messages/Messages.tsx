2dc5139adc0aadaaa79b0d2aefb7da6a45be9c3b
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-23 21:54:57 +0600

    fEnd: Added layout to Messages

--- frontend/src/features/messages/Messages.tsx
+++ frontend/src/features/messages/Messages.tsx
@@ -1,5 +1,16 @@
+import { Grid, Typography } from "@mui/material";
+
 const Messages = () => {
-  return <div>Messages</div>;
+  return (
+    <Grid container direction="column" spacing={2}>
+      <Grid item justifyContent="space-between" alignItems="center">
+        <Typography variant="h4">Messages</Typography>
+      </Grid>
+      <Grid item container direction='column' alignItems='center' spacing={2}>
+        Messages' list
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Messages;
