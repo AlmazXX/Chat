ceba47bc4cb61682233b1d3fd23b8acf32f2433f
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-23 23:31:00 +0600

    fEnd: Added rendering of messages to Messages

--- frontend/src/features/messages/Messages.tsx
+++ frontend/src/features/messages/Messages.tsx
@@ -1,13 +1,34 @@
 import { Grid, Typography } from "@mui/material";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import MessageItem from "./components/MessageItem";
+import { selectMessages } from "./messagesSlice";
+import { fetchMessages } from "./messagesThunk";
 
 const Messages = () => {
+  const dispatch = useAppDispatch();
+  const messages = useAppSelector(selectMessages);
+
+  useEffect(() => {
+    let datetime = "";
+    const interval = setInterval(async () => {
+      await dispatch(fetchMessages(datetime));
+      datetime = messages.length ? messages[messages.length - 1].datetime : "";
+    }, 2000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [dispatch, messages]);
+
   return (
     <Grid container direction="column" spacing={2}>
       <Grid item justifyContent="space-between" alignItems="center">
         <Typography variant="h4">Messages</Typography>
       </Grid>
-      <Grid item container direction='column' alignItems='center' spacing={2}>
-        Messages' list
+      <Grid item container direction="column" alignItems="center" spacing={2}>
+        {messages.map((message) => (
+          <MessageItem key={message.id} message={message} />
+        ))}
       </Grid>
     </Grid>
   );
