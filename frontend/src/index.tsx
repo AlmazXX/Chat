514d4c2777afe4cae914c8abb57086a710c4ffb4
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-23 21:35:05 +0600

    fEnd: Added Provider BrowserRouter ThemeProvider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,24 @@
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { ThemeProvider } from "@mui/material";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { store } from './app/store';
+import theme from "./theme";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
