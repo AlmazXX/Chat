08d49e03175ebdd40bf436711c999bdf31051b81
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-23 21:43:00 +0600

    fEnd: Added layout to App Added route to Messages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
+import { Container, CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import AppToolbar from "./components/UI/AppToolbar/AppToolbar";
+import Messages from "./features/messages/Messages";
+
 function App() {
   return (
     <>
-      App
+      <CssBaseline />
+      <header>
+        <AppToolbar />
+      </header>
+      <main>
+        <Container maxWidth="xl">
+          <Routes>
+            <Route path="/" element={<Messages />} />
+          </Routes>
+        </Container>
+      </main>
     </>
   );
 }
