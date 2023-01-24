2418d0688ede3656251e8ed0277f6bd2d1b8d737
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-24 12:10:15 +0600

    fEnd: Added sendMessage to messagesSlice

--- frontend/src/features/messages/messagesSlice.ts
+++ frontend/src/features/messages/messagesSlice.ts
@@ -1,18 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { Message } from "../../../../types";
 import { RootState } from "../../app/store";
-import { fetchMessages } from "./messagesThunk";
+import { fetchMessages, sendMessage } from "./messagesThunk";
 
 interface MessagesState {
   items: Message[];
   fetchLoading: boolean;
   datetime: string;
+  sending: boolean;
 }
 
 const initialState: MessagesState = {
   items: [],
   fetchLoading: false,
   datetime: "",
+  sending: false,
 };
 
 export const messagesSlice = createSlice({
@@ -31,6 +33,15 @@ export const messagesSlice = createSlice({
       })
       .addCase(fetchMessages.rejected, (state) => {
         state.fetchLoading = false;
+      })
+      .addCase(sendMessage.pending, (state) => {
+        state.sending = true;
+      })
+      .addCase(sendMessage.fulfilled, (state) => {
+        state.sending = false;
+      })
+      .addCase(sendMessage.rejected, (state) => {
+        state.sending = false;
       });
   },
 });
@@ -40,3 +51,5 @@ export const selectMessages = (state: RootState) => state.messages.items;
 export const selectMessagesFetching = (state: RootState) =>
   state.messages.fetchLoading;
 export const selectDatetime = (state: RootState) => state.messages.datetime;
+export const selectMessageSending = (state: RootState) =>
+  state.messages.sending;
