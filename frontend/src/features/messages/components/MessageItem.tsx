eeb9fcc1661b0e223051ea54065ee448bdcf779c
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-24 12:51:23 +0600

    fEnd: Added dayjs to MessageItem

--- frontend/src/features/messages/components/MessageItem.tsx
+++ frontend/src/features/messages/components/MessageItem.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, Grid, Typography } from "@mui/material";
+import dayjs from "dayjs";
 import { FC } from "react";
 import { Message } from "../../../../../types";
 
@@ -6,13 +7,17 @@ interface Props {
   message: Message;
 }
 
-const MessageItem: FC<Props> = ({message}) => (
-  <Grid item sx={{width: '100%'}}>
+const MessageItem: FC<Props> = ({ message }) => (
+  <Grid item sx={{ width: "100%" }}>
     <Card>
       <CardContent>
         <Typography variant="h5">{message.author}</Typography>
-        <Typography variant="body2" color='text.secondaty' sx={{mb:1.5}}>{message.datetime}</Typography>
-        <Typography variant="body2" color='text.secondaty'>{message.message}</Typography>
+        <Typography variant="body2" color="text.secondaty" sx={{ mb: 1.5 }}>
+          {dayjs(message.datetime).format("DD.MM.YYYY HH:mm")}
+        </Typography>
+        <Typography variant="body2" color="text.secondaty">
+          {message.message}
+        </Typography>
       </CardContent>
     </Card>
   </Grid>
