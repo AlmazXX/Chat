3da620049825d495cf4915941e301e2effa64e35
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-24 11:42:17 +0600

    fEnd: Updated fetchMessages

--- frontend/src/features/messages/messagesThunk.ts
+++ frontend/src/features/messages/messagesThunk.ts
@@ -1,11 +1,17 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { Message } from "../../../../types";
+import { RootState } from "../../app/store";
 import axiosApi from "../../axiosApi";
 
-export const fetchMessages = createAsyncThunk<Message[], string>(
-  "messages/fetchAll",
-  async (datetime) => {
-    const { data } = await axiosApi.get(`/messages?datetime=${datetime}`);
-    return data;
-  }
-);
+export const fetchMessages = createAsyncThunk<
+  Message[],
+  undefined,
+  { state: RootState }
+>("messages/fetchAll", async (_, ThunkAPI) => {
+  const datetime = ThunkAPI.getState().messages.datetime;
+  const { data } = await axiosApi.get<Message[] | null>(
+    `/messages?datetime=${datetime}`
+  );
+
+  return data ? data : [];
+});
