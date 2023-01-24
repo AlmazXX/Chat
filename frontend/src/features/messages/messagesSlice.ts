2c1a9f02b371f6dad7f5c06742e345f000d23a16
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-24 12:58:30 +0600

    fEnd: Removed fetchLoading state in messagesSlice

--- frontend/src/features/messages/messagesSlice.ts
+++ frontend/src/features/messages/messagesSlice.ts
@@ -5,14 +5,12 @@ import { fetchMessages, sendMessage } from "./messagesThunk";
 
 interface MessagesState {
   items: Message[];
-  fetchLoading: boolean;
   datetime: string;
   sending: boolean;
 }
 
 const initialState: MessagesState = {
   items: [],
-  fetchLoading: false,
   datetime: "",
   sending: false,
 };
@@ -23,19 +21,12 @@ export const messagesSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchMessages.pending, (state) => {
-        state.fetchLoading = true;
-      })
       .addCase(fetchMessages.fulfilled, (state, { payload: messages }) => {
-        state.fetchLoading = false;
         state.datetime = messages[messages.length - 1].datetime;
         state.items = [...state.items, ...messages];
         state.items.sort((a, b) =>
           new Date(a.datetime) > new Date(b.datetime) ? -1 : 1
         );
-      })
-      .addCase(fetchMessages.rejected, (state) => {
-        state.fetchLoading = false;
       })
       .addCase(sendMessage.pending, (state) => {
         state.sending = true;
@@ -51,8 +42,6 @@ export const messagesSlice = createSlice({
 
 export const messagesReducer = messagesSlice.reducer;
 export const selectMessages = (state: RootState) => state.messages.items;
-export const selectMessagesFetching = (state: RootState) =>
-  state.messages.fetchLoading;
 export const selectDatetime = (state: RootState) => state.messages.datetime;
 export const selectMessageSending = (state: RootState) =>
   state.messages.sending;
