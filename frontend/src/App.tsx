9979e4a3e9d0675644a2d7f5613274f04c60b431
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-24 12:11:53 +0600

    fEnd: Created onSubmit function in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
 import { Container, CssBaseline, Grid } from "@mui/material";
+import { MessageMutation } from "../../types";
+import { useAppDispatch } from "./app/hooks";
 import AppToolbar from "./components/UI/AppToolbar/AppToolbar";
+import MessageForm from "./features/messages/components/MessageForm";
 import Messages from "./features/messages/Messages";
+import { sendMessage } from "./features/messages/messagesThunk";
 
 function App() {
+  const dispatch = useAppDispatch()
+
+  const onSubmit = (message: MessageMutation) => {
+    dispatch(sendMessage(message))
+  }
+
   return (
     <>
       <CssBaseline />
@@ -12,6 +22,9 @@ function App() {
       <main>
         <Container maxWidth="xl">
           <Grid container direction="column" spacing={2}>
+            <Grid item>
+              <MessageForm onSubmit={onSubmit} />
+            </Grid>
             <Grid item>
               <Messages />
             </Grid>
